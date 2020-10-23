9456a76ca567c18532e66aed886b2347a61cdc77
winktim <twinkelmann@protonmail.com>
2020-10-23 15:48:13 +0200

    Updated presentation

--- sections/section-catherine-charmillot.vue
+++ sections/section-catherine-charmillot.vue
@@ -8,26 +8,24 @@
   >
     <div class="flex flex-col lg:flex-row lg:mb-4">
       <div>
-        <h3 class="font-bold pb-1">Reconnaissances</h3>
         <ul class="list-disc pl-6">
           <li>
-            Diplôme d’études avancées (DAS) en santé sexuelle et reproductive
-            (2006-2008)
+            Diplôme d’études avancées (DAS) en santé sexuelle et reproductive,
+            volet éducation (2006-2008)
           </li>
-          <li>Éducatrice et conseillère en santé sexuelle (2013)</li>
-          <li>Formatrice en santé sexuelle (2014)</li>
-          <li>
-            Sexologue 2ème niveau de formation en sexocorporel (2016-2020)
-          </li>
+          <li>Formation passerelle volet conseil en santé sexuelle (2013)</li>
+          <li>Formatrice en santé sexuelle (2015)</li>
+          <li>Sexologue 2 niveaux de formation en sexocorporel (2016-2020)</li>
         </ul>
+
         <br />
         <p>
-          J’anime avec beaucoup d’enthousiasme des cours d’éducation sexuelle
-          dans les écoles pour la promotion et la prévention de la santé
-          sexuelle.<br />
-          Je travaille également comme conseillère en santé sexuelle au centre
-          de santé sexuelle-planning familial jurassien pour informer, orienter
-          et soutenir les personnes qui consultent.
+          Depuis 2008, j’anime avec beaucoup d’enthousiasme des cours
+          d’éducation sexuelle dans les écoles pour la promotion et la
+          prévention de la santé sexuelle.<br />
+          Depuis 2015, je travaille comme conseillère en santé sexuelle au
+          centre de santé sexuelle-planning familial jurassien pour informer,
+          orienter et soutenir les personnes qui consultent.
         </p>
         <br />
         <p>
