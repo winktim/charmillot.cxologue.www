430f9f1c6a54cd5b0cd332f042d22d362dc63379
winktim <twinkelmann@protonmail.com>
2020-10-24 00:04:07 +0200

    Fixed background on Safari iOS and Firefox Mobile Changed position of logo

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -11,16 +11,15 @@
         d="m 0,37.169735 30,21.662986 c 30,21.895301 90,64.872799 150,80.553789 60,15.68098 120,2.90388 180,-15.50675 60,-18.7591 120,-43.151738 180,-49.54029 60,-6.388541 120,6.388552 180,0 60,-6.388541 120,-30.781183 180,-37.169735 60,-6.388541 120,6.388552 180,21.662986 60,15.506749 120,34.091617 180,24.799188 60,-9.292439 120,-46.462174 150,-65.047041 L 1440,0 v 167.26381 h -30 c -30,0 -90,0 -150,0 -60,0 -120,0 -180,0 -60,0 -120,0 -180,0 -60,0 -120,0 -180,0 -60,0 -120,0 -180,0 -60,0 -120,0 -180,0 -60,0 -120,0 -180,0 -60,0 -120,0 -150,0 H 0 Z"
       />
     </svg>
-    <div class="absolute w-full h-full z-bottom bg-fixed footer-image"></div>
     <div class="h-full flex flex-col items-center justify-between py-12">
       <img
-        class="w-2/3 md:w-100 mx-12 select-none mt-48"
+        class="w-2/3 md:w-100 mx-12 select-none mt-12 lg:mt-48"
         src="/images/logo-colors.svg"
         alt="C'XOLOGUE"
       />
 
       <p
-        class="w-60 text-gray-100 flex flex-col mx-4 text-shadow-md md:self-start md:ml-12 md:mt-12 lg:ml-48 xl:ml-80"
+        class="w-60 text-gray-100 flex flex-col mx-4 text-shadow-md md:self-start mt-36 md:ml-12 md:mt-48 lg:mt-12 lg:ml-48 xl:ml-80"
       >
         <span class="font-bold italic text-center"
           >«&nbsp;Le sexe apaise les tensions. L'amour les provoque&nbsp;»</span
