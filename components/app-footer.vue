54aa1e611c5837348df0eaeaa01f407581bbe57e
winktim <twinkelmann@protonmail.com>
2020-10-23 17:59:56 +0200

    Added quote

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -4,13 +4,22 @@
       class="w-full h-screen md:h-big-screen absolute z-bottom bg-fixed md:bg-local md:parallax-background footer-image"
     ></div>
     <div
-      class="absolute z-bottom w-full h-screen md:parallax-footer flex flex-col items-center justify-around"
+      class="absolute z-bottom w-full h-screen md:parallax-footer flex flex-col items-center justify-between"
     >
       <img
-        class="w-2/3 md:w-100 mx-12 flex-grow select-none"
+        class="w-2/3 md:w-100 mx-12 select-none mt-48"
         src="/images/logo-colors.svg"
         alt="C'XOLOGUE"
       />
+
+      <p
+        class="text-gray-100 flex flex-col mx-4 text-shadow-md md:self-start md:ml-12 md:mt-48 xl:ml-80 md:w-60"
+      >
+        <span class="font-bold italic text-center"
+          >«&nbsp;Le sexe apaise les tensions. L'amour les provoque&nbsp;»</span
+        >
+        <span class="text-right">Woody Allen</span>
+      </p>
 
       <p
         class="text-center w-2/3 lg:w-160 text-gray-100 text-shadow-md mb-8 md:-mb-12"
